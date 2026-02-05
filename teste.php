8e09f1289e035757a2790f6a27829bc4160c9c17
DevKalleby <kallebyrodrigues123@gmail.com>
2026-02-05 13:04:34 -0300

    adicionando a subtracao e multiplicacao

--- teste.php
+++ teste.php
@@ -3,3 +3,13 @@ $a = 5;
 $b = 10;
 $sum = $a + $b;
 echo "The sum of $a and $b is $sum.";
+
+$c = 10;
+$d = 5;
+$subtracao = $c - $d;
+echo "The subtraction of $c and $d is $subtracao.";
+
+$e = 4;
+$f = 2;
+$multiplicacao = $e * $f;
+echo "The multiplication of $e and $f is $multiplicacao.";
